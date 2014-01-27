851d35ed91385bfb4c5dbbb310d317b8a4facc5a
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2014-01-26 18:31:51 -0800

    Added config for validator StreamTask
    Fixed issue with timestamp field parsing in original Feed StreamTask
    Changed Feed task config to input from Kafka instead of my own terrible
    system implementation.

--- samza-medical/src/main/java/poltak/samza/medicaldata/task/MedicalDataFeedStreamTask.java
+++ samza-medical/src/main/java/poltak/samza/medicaldata/task/MedicalDataFeedStreamTask.java
@@ -9,6 +9,9 @@ import org.apache.samza.task.TaskCoordinator;
 import poltak.samza.medicaldata.util.InvalidMedicalDataException;
 import poltak.samza.medicaldata.util.MedicalData;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class MedicalDataFeedStreamTask implements StreamTask
 {
   private static final SystemStream OUTPUT_STREAM       = new SystemStream("kafka", "medical-test");
@@ -33,7 +36,7 @@ public class MedicalDataFeedStreamTask implements StreamTask
     messageCollector.send(new OutgoingMessageEnvelope(OUTPUT_STREAM, dataReceived));
   }
 
-  private MedicalData parseLineOfData(final String line) throws InvalidMedicalDataException
+  private MedicalData parseLineOfData(final String line) throws InvalidMedicalDataException, ParseException
   {
     String[] fields = line.split(VALID_CSV_DELIMITER);
 
@@ -42,7 +45,7 @@ public class MedicalDataFeedStreamTask implements StreamTask
 
     return new MedicalData(
         fields[0],
-        null, // TODO: fields[1],
+        new SimpleDateFormat(VALID_TIME_FORMAT).parse(fields[1]),
         Integer.parseInt(fields[2]),
         Integer.parseInt(fields[3]),
         Integer.parseInt(fields[4]),
