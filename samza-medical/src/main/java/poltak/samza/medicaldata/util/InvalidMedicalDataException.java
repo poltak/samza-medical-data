6b07e1b17e91e11cdec2539b2e6aed14b152a99f
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2014-01-28 11:45:52 -0800

    Removed unused constructor from custom Exception

--- samza-medical/src/main/java/poltak/samza/medicaldata/util/InvalidMedicalDataException.java
+++ samza-medical/src/main/java/poltak/samza/medicaldata/util/InvalidMedicalDataException.java
@@ -2,11 +2,6 @@ package poltak.samza.medicaldata.util;
 
 public class InvalidMedicalDataException extends Exception
 {
-  public InvalidMedicalDataException()
-  {
-    super();
-  }
-
   public InvalidMedicalDataException(final String message)
   {
     super(message);
